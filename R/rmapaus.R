80a4ef35e5e3bd5b88bd32a748e2d57c0c256b6c
Sean Carmody <seancarmody@gmail.com>
2013-11-27 17:33:31 +1100

    Add map documentation

--- R/rmapaus.R
+++ R/rmapaus.R
@@ -8,6 +8,7 @@ NULL
 #' Statistical division map data
 #'  
 #' A dataset containing map data for Australian statistical divisions (SD). 
+#' Data is sourced from the Australian Bureau of Statistics.
 #' 
 #' @docType data
 #' @keywords datasets
@@ -32,6 +33,7 @@ NULL
 #' Statistical sub-division map data
 #'  
 #' A dataset containing map data for Australian statistical sub-divisions (SSD). 
+#' Data is sourced from the Australian Bureau of Statistics.
 #' 
 #' @docType data
 #' @keywords datasets
@@ -51,4 +53,65 @@ NULL
 #' @name SSD.smoothed
 #' @usage data(ssd)
 #' @format A spatial polygon data frame with 206 regions
+NULL
+
+#' Statistical sub-division map data
+#'  
+#' A dataset containing map data for Australian statistical sub-divisions (SSD). 
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name SSD.full
+#' @usage data(ssd)
+#' @format A spatial polygon data frame with 206 regions
+NULL
+
+#' State and Territory map data
+#'  
+#' A dataset containing map data for Australian states and territories.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name STE.full
+#' @usage data(ste)
+#' @format A spatial polygon data frame with 206 regions
+NULL
+
+#' State and Territory mapdata (smoothed)
+#'  
+#' A dataset containing map data for Australian states and territories
+#' The regions have been adapted from the full ABS specification by smoothing
+#' the boundaries to reduce the size of the data set and speed up plotting.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name STE.smoothed
+#' @usage data(ste)
+#' @format A spatial polygon data frame with 206 regions
+NULL
+
+#' State and Territory mapdata (caricature)
+#'  
+#' A dataset containing map data for Australian states and territories
+#' The regions have been adapted from the full ABS specification by smoothing
+#' the boundaries to reduce the size of the data set and speed up plotting.
+#' These regions have a higher level of smoothing than the "smoothed" data set.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name STE.caricature
+#' @usage data(ste)
+#' @format A spatial polygon data frame with 206 regions
+NULL
+
+#' Statistical local area map data
+#'  
+#' A dataset containing map data for Australian statistical local areas (SLA).
+#' Data is sourced from the Australian Bureau of Statistics.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name SLA.full
+#' @usage data(sla)
+#' @format A spatial polygon data frame with 61 regions
 NULL
