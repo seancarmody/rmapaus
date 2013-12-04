e73e431db38e3a3569b5618beb3730fe812d8ab9
Sean Carmody <seancarmody@gmail.com>
2013-12-04 19:56:20 +1100

    Export scale functions and bump version number

--- R/scales.R
+++ R/scales.R
@@ -10,12 +10,22 @@ scale_fill_heat <- function(...){
                        labels = percent, ...)
 }
 
+#' Add guide colours
+#' 
+#' @param ... additional arguments passed to scale functions
+#' 
+#' @export
 scale_colour_heat <- function(...){
   scale_colour_gradient2(na.value = "grey65", 
                          low = "royalblue1", high ="firebrick1",
                          labels = percent, ...)
 }
 
+#' Add guide size
+#'  
+#' @param ... additional arguments passed to scale functions
+#' 
+#' @export
 scale_colour_size <- function(colour = "black", ...){
   guides(size=guide_legend(override.aes=list(colour=colour), ...))
 }
